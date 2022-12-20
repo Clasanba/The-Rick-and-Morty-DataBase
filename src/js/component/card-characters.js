73e7c407f72b40d3c0d41f59977643c22b699f21
Clasanba <clasanba@gmail.com>
2022-12-20 17:02:08 +0000

    card characters done

--- src/js/component/card-characters.js
+++ src/js/component/card-characters.js
@@ -7,20 +7,18 @@ export const CharaExtend = ({ character }) => {
 
   return (
     <>
-      <div className="card mb-3 bckgrnd-cards bg-gradient">
-        <div className="row g-0">
+      <div className="card bg-gradient rounded-5 m-5 box-card-extend" >
+        <div className="row g-0" >
           <div className="col-md-4">
-            <img src={character.image} className="img-fluid rounded-start" alt="..." />
+            <img src={character.image} className="img-fluid" alt="..." />
           </div>
           <div className="col-md-8  ">
-            <div className="card-body">
-              <h5 className="card-title">{character.name}</h5>
-              <p className="card-text" >Estado:{character.status}</p>
-              <p className="card-text" >Especie:{character.species}</p>
-              <p className="card-text" >Genero:{character.gender}</p>
-              <p className="card-text" >Origen:{character.origin?.name}</p>
-                
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            <div className="card-body ">
+              <h5 className="fs-1 fw-bold  text-white">{character.name}</h5>
+              <p className="card-title   text-white">is...  {character.status}</p>
+              <p className="card-text   text-white" >and is...  {character.species}</p>
+              <p className="card-text text-white" >Gender...  {character.gender}</p>
+              <p className="card-text  text-white" >Origin...  {character.origin?.name}</p>
             </div>
           </div>
         </div>
--- src/js/component/home-location.js
+++ src/js/component/home-location.js
@@ -6,11 +6,11 @@ export const LocationHome = ({ location, onAddFavorite, isFavorite, deleteFav })
 
 
     return (<>
-        <div className="card-body bckgrnd-cards-locat bg-gradient rounded">
+        <div className="card-body bckgrnd-cards-locat bg-gradient rounded mb-2">
             <div className="" >
-                <div className="card-title fs-3 text-dark mb-4"><h5 className="">{location.name}</h5></div>
-                <div className="d-flex text-cards"><p className="   text-start ms-4 fw-bold me-3">What is that?</p><p>{location.type}</p></div>
-                <div className="d-flex text-cards"><p className="  text-start ms-4 fw-bold me-3">Dimension</p><p className=" ">{location.dimension}</p></div>
+                <div className="card-title fs-3 text-dark mb-5 "><h5 className="">{location.name}</h5></div>
+                <div className="d-flex text-cards mb-1"><p className=" text-start  fw-bold me-1">What is that?</p><p>{location.type}</p></div>
+                <div className="d-flex text-cards mb-3"><p className="  text-start ms-2 fw-bold me-1">Dimension</p><p className=" ">{location.dimension}</p></div>
             </div>
             <div className=" btnLinks"> 
                 {!isFavorite ? (<Link to="" onClick= {() => onAddFavorite(location)} className="card-link fa-regular fa-face-grin-hearts text-decoration-none fs-3 text-dark"></Link>) : (<Link to="" onClick= {() => deleteFav(location)} className="card-link fa-solid fa-poop text-decoration-none fs-3 text-dark"></Link>)}
