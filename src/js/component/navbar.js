b30052e45c701125f26ea869d6ed4d1bd4aae976
Clasanba <clasanba@gmail.com>
2023-01-16 17:49:58 +0000

    complete design btn favorite

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,6 +4,8 @@ import layout from "../layout";
 import RickMortilogo from "../../img/Rick-Morti-logo.png";
 import "../../styles/index.css";
 import  "../../styles/navBar.css";
+import Home from "../views/home.js";
+
 
 
 const linkSomePlace = (favorite)=> {
@@ -12,7 +14,8 @@ const linkSomePlace = (favorite)=> {
 	}else{ return `/locextend/${favorite.id}`}
 }
 
-export const Navbar = ({favorites, deleteFavorites}) => {
+
+export const Navbar = ({favorites,deleteFavorites}) => {
 	
 
 	return (
@@ -23,15 +26,15 @@ export const Navbar = ({favorites, deleteFavorites}) => {
 			</Link>
 			</div>
 			<div className="me-2">
-					<div className="dropdown">
-						<button className="btn btnFavorite text-decoration-none fw-bold rounded-pill p-2" disabled={favorites.length===0} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+					<div className="btn-group">
+						<button className="btn btnFavorite text-decoration-none fw-bold rounded-pill p-2 bg-gradient" disabled={favorites.length===0} type="button" data-bs-toggle="dropdown" aria-expanded="false">
 							Favoritos {favorites.length}
 						</button>
-							<ul className="dropdown-menu list-group-flush luFavorite rounded rounded-3">
+							<ul className="dropdown-menu dropdown-menu-end luFavorite rounded rounded-3">
 							{
 							favorites.map ((favorite, i)=>{ 
 								return	(
-									<li key={i} className=" d-flex mx-2 liFavorite dropdown-item"><Link to={linkSomePlace(favorite)} className="text-decoration-none liFavorite"><p className="card-title ">{favorite.name}</p></Link><button type="button" className=" fa-regular fa-trash-can border border-0 bg-transparent" aria-label="Delete" onClick={() => deleteFavorites(i)}></button></li>
+									<li key={i} className="d-flex liFavorite dropdown-item"><Link to={linkSomePlace(favorite)} className="text-decoration-none liFavorite"><p className="card-title ">{favorite.name}</p></Link><button type="button" className=" fa-regular fa-trash-can border border-0 bg-transparent btnDeleteFav pb-2" aria-label="Delete" onClick={() => deleteFavorites(i)}></button></li>
 									)
 							})
 							}
