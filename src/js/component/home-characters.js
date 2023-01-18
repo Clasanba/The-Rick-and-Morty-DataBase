914ac70e41219c3c3f25f7cf09d945ea21a7d73d
Clasanba <clasanba@gmail.com>
2023-01-18 12:08:37 +0000

    design cards location

--- src/js/component/home-characters.js
+++ src/js/component/home-characters.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../../styles/index.css";
 
 export const CharactersHome = ({ character, onAddFavorite, isFavorite, deleteFav }) => {
 
@@ -11,7 +12,7 @@ export const CharactersHome = ({ character, onAddFavorite, isFavorite, deleteFav
             <img src={character.image} className="card-img-top img-card rounded-circle bg-gradient" alt="..." />
             <div className="card-body" >
             <Link to={`/charextend/${character.id}`}  className="text-decoration-none fs-3 icons">
-                <h5 className="card-title fs-3 bg-gradient rounded rounded-4 mb-1">{character.name}</h5>
+                <h5 className="fs-3 bg-gradient rounded rounded-4 title-card">{character.name}</h5>
                 </Link>
             </div>
             <div className="card-body btnLinks"> 
--- src/js/component/home-location.js
+++ src/js/component/home-location.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../../styles/index.css";
 
 export const LocationHome = ({ location, onAddFavorite, isFavorite, deleteFav }) => {
 
 
 
     return (<>
-        <div className="card-body bckgrnd-cards-locat bg-gradient rounded mb-2">
+        <div className="card bckgrnd-cards-locat bg-gradient mt-5 rounded rounded-4">
             <div className="" >
-                <div className="card-title fs-3 bg-gradient mb-4 "><h5 className="">{location.name}</h5></div>
-                <div className="d-flex text-cards mb-1"><p className=" text-start  fw-bold me-1">What is that?</p><p>{location.type}</p></div>
-                <div className="d-flex text-cards mb-3"><p className=" text-start ms-2 fw-bold me-1">Dimension</p><p className=" ">{location.dimension}</p></div>
+            <Link to={`/locextend/${location.id}`} className="text-decoration-none fs-3 icons">
+                <div className="card-title fs-3 bg-gradient mt-4 ms-2 ">
+                    <h5 className="fs-3 bg-gradient rounded rounded-4 title-card">{location.name}</h5>
+                </div>
+            </Link>
             </div>
-            <div className=" btnLinks"> 
-                {!isFavorite ? (<Link to="" onClick= {() => onAddFavorite(location)} className="card-link fa-regular fa-face-grin-hearts text-decoration-none fs-3 text-dark"></Link>) : (<Link to="" onClick= {() => deleteFav(location)} className="card-link fa-solid fa-poop text-decoration-none fs-3 text-dark"></Link>)}
-                <Link to={`/locextend/${location.id}`} className="card-link fa-solid fa-circle-plus text-decoration-none fs-3 text-dark"></Link>
+            <div className="body-card">
+                <div className="d-flex text-cards"><p>{location.type}</p></div>
+                <div className="card-body btnLinks"> 
+                    {!isFavorite ? (<Link to="" onClick= {() => onAddFavorite(location)}
+                    className="card-link fa-regular fa-face-grin-hearts icons text-decoration-none fs-3"></Link>) : 
+                    (<Link to="" onClick= {() => deleteFav(location)} 
+                    className="fa-regular fa-face-frown card-link text-decoration-none icons fs-3"></Link>)}
+                </div> 
             </div>
         </div>
 
