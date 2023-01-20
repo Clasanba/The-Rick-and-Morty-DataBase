559cac312ed70ce540cf9f93563d7e690d30acaa
Clasanba <clasanba@gmail.com>
2023-01-20 16:55:40 +0000

    responsive complete

--- src/js/component/home-characters.js
+++ src/js/component/home-characters.js
@@ -6,17 +6,20 @@ export const CharactersHome = ({ character, onAddFavorite, isFavorite, deleteFav
 
 
     return (<>
-
-        
-        <div className="card bckgrnd-cards-charact bg-gradient mt-5 rounded rounded-4">
-            <img src={character.image} className="card-img-top img-card rounded-circle bg-gradient" alt="..." />
-            <div className="card-body" >
-            <Link to={`/charextend/${character.id}`}  className="text-decoration-none fs-3 icons">
-                <h5 className="fs-3 bg-gradient rounded rounded-4 title-card">{character.name}</h5>
-                </Link>
+    
+        <div className="card bckgrnd-cards-charact bg-gradient rounded rounded-4 ">
+            <div className="">
+            <img src={character.image} className="card-img-top img-card rounded-circle bg-gradient " alt="..." />
             </div>
-            <div className="card-body btnLinks"> 
+            <div className="card-body">
+            <div className="">
+            <Link to={`/charextend/${character.id}`} className="text-decoration-none fs-3 icons">
+                <h5 className=" card-title fs-3 bg-gradient rounded rounded-4 title-card">{character.name}</h5>
+            </Link>
+            </div>
+            <div className="btnLinks"> 
                 { !isFavorite ? (<Link to="" onClick= {() => onAddFavorite(character)} className="card-link fa-regular fa-face-grin-hearts icons text-decoration-none fs-3 "></Link>) : (<Link to="" onClick= {() => deleteFav(character)} className="fa-regular fa-face-frown card-link text-decoration-none icons fs-3 "></Link>)}
+            </div>
             </div>
         </div>
 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -45,18 +45,18 @@ const Home = ({favorites, setFavorites}) => {
 
 		, [])
 
-	return <div className=" bg-gradient">
+	return <div className="bg-gradient">
 		
 		<div className="">
-			<h1 className="text-center  title-character fs-1 fw-semibolder bg-gradient pt-5 text-capitalize">Characters</h1>
+			<h1 className="text-center title-character fs-1 fw-semibolder bg-gradient pt-5 text-capitalize">Characters</h1>
 		</div>
-		<div className="container text-center cardCharacters ">
-			<div className="row align-items-start ">
-
+		<div className="container text-center">
+			<div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3  ">
+			
 				{characters.map((character, index) => {
-					return <div className=" col-md-3 "> <CharactersHome isFavorite={favorites.includes(character)} onAddFavorite={addFavorites} deleteFav={deleteFavorites} character={character} key={index} /></div>
+					return <div className="col"> <CharactersHome isFavorite={favorites.includes(character)} onAddFavorite={addFavorites} deleteFav={deleteFavorites} character={character} key={index} /></div>
 				})}
-
+			
 			</div>
 		
 		</div>
@@ -65,10 +65,10 @@ const Home = ({favorites, setFavorites}) => {
 			<h1 className="text-center bg-gradient title-location fs-1 fw-semibolder text-capitalize mt-5">Locations</h1>
 		</div>
 		<div className="container text-center cardCharacters">
-			<div className="row align-items-start">
+			<div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
 
 				{locations.map((location, index) => {
-					return <div className="col-md-3 "><LocationHome isFavorite={favorites.includes(location)} onAddFavorite={addFavorites} deleteFav={deleteFavorites} location={location} key={index} /></div>
+					return <div className="col "><LocationHome isFavorite={favorites.includes(location)} onAddFavorite={addFavorites} deleteFav={deleteFavorites} location={location} key={index} /></div>
 				})}
 			</div>
 		</div>
